632531dc55c51daeba1ddd8feb9fd7d923f19462
Utku Kaan <tutkuceren15@gmail.com>
2017-09-12 09:08:26 +0300

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected!');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined, give him some hugs!',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('Message sent from client side:', message);
         io.emit('newMessage', {
@@ -20,6 +32,12 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime()
         });
+
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {

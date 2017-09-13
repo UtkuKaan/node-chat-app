fcc40dedc15fc21ee9b39c26d6e993a19207b95f
Utku Kaan <tutkuceren15@gmail.com>
2017-09-13 12:16:02 +0300

    Modify message utils to use momentjs

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,14 @@
+var moment = require('moment');
+
 var generateMessage = (from, text) => {
-    return {from, text, createdAt: new Date().getTime()};
+    return {from, text, createdAt: moment().valueOf()};
 };
 
 var generateLocationMessage = (from, lat, lon) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${lat},${lon}`,
-        createdAt: new Date().getTime()
+        createdAt: moment().valueOf()
     };
 };
 

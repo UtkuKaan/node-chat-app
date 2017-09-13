da8eed8dad988383e2a39758ad36f62bf5d6388b
Utku Kaan <tutkuceren15@gmail.com>
2017-09-13 11:38:47 +0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -30,11 +30,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
     e.preventDefault();
 
+    var messageTextbox = jQuery('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message]').val()
+        text: messageTextbox.val()
     }, function () {
-
+        messageTextbox.val('');
     });
 });
 
@@ -43,12 +45,17 @@ locationButton.on('click', function () {
     if (!navigator.geolocation) {
         return alert('Geolocation is not supported by your browser.');
     }
+
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (pos) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: pos.coords.latitude,
             longitude: pos.coords.longitude
         });
     }, function () {
-        alert('Unable to fetch location.')
+        locationButton.removeAttr('disabled').text('Send location');
+        alert('Unable to fetch location.');
     });
 });
